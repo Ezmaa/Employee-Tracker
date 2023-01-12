b8523481018cbab406f67b444891275d4f2944f0
ezmaa <ezinmark@gmail.com>
2023-01-12 08:45:10 -0500

    wrote code for folders and add connection js folder

--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,33 +1,31 @@
-INSERT INTO departments(department_name)
-VALUES 
-('Management'),
-('Sales'),
-('Engineering'),
-('Human Resources'),
-('Finance'),
-('Legal'),
-('Design');
+INSERT INTO department (name)
+VALUES ('Management'),
+       ('Sales'),
+       ('Engineering'),
+       ('Human Resources'),
+       ('Finance'),
+       ('Legal'),
+       ('Design');
 
-INSERT INTO roles(title, salary, department_id)
-VALUES
-('Regional Manager', 150000, 1),
-('Sales Rep', 85000, 2),
-('HR Rep', 90000, 3),
-('Software Engineer', 145000, 4),
-('Ux/Ui Designer', 110000, 5),
-('Accountant', 89000, 6);
-('Lawyer', 120000, 7);
-('Tech Lead', 210000, 8);
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Regional Manager', 150000, 4),
+       ('Sales Rep', 85000, 4),
+       ('HR Rep', 90000, 1),
+       ('Software Engineer', 145000, 2),
+       ('Ux/Ui Designer', 110000, 3),
+       ('Accountant', 89000, 3),
+       ('Lawyer', 120000, 3),
+       ('Tech Lead', 210000, 3);
 
-INSERT INTO employees(first_name, last_name, role_id) 
-VALUES
-('Denzel', 'Washington', 8),
-('Damson', 'Idris', 5),
-('Idris', 'Elba', 2),
-('Jason', 'Stratham', 7),
-('Kate', 'Hudson', 6),
-('Nicole', 'Anniston', 3);
-('Cameron', 'Diaz', 4);
-('Chritian', 'Bale', 1);
-('Gerald', 'Butler', 4);
-UPDATE `employee_db`.`employees` SET `manager_id` = '1' WHERE (`id` > '1');
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES ('Denzel', 'Washington', 1, NULL),
+       ('Damson', 'Idris', 2, 1),
+       ('Idris', 'Elba', 2, 1),
+       ('Jason', 'Stratham', 2, NULL),
+       ('Kate', 'Hudson', 3, NULL),
+       ('Nicole', 'Anniston', 4, 5),
+       ('Cameron', 'Diaz', 5, 5),
+       ('Chritian', 'Bale', 6, NULL),
+       ('Gerald', 'Butler', 7, 8),
+       ('Ryan', 'Gosling', 7, 8);
